aa29f66216abe89d86a2e7ce10d7c47802703ab8
dianajyoo <diana.yooj@gmail.com>
2018-10-01 16:35:49 -0400

    edited run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,14 @@
 require_relative '../config/environment'
-
+require 'pry'
 
 
 puts "HELLO WORLD"
+
+puts "What is your full name? "
+name = gets.chomp
+User.find_or_create_by(name)
+
+puts "Hi, #{user_name.name}"
+
+binding.pry
+0
--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+  def self.find_or_create_by(name)
+    if User.count == 0 || self.find_by_name(name) == nil
+      self.create(name: name)
+    else
+      self.all.find do |user|
+        user.name == name
+      end
+    end
+    "Hi, #{name}!"
+  end
+
 end
