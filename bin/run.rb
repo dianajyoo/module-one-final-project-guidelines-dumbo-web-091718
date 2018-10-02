2e84b22c17d5387405ec99db3b5c72d3af14ed93
Brian DiRito <bdirito@gmail.com>
2018-10-02 09:54:18 -0400

    Did some refactoring, option 2 adds to my_coffees

--- bin/run.rb
+++ bin/run.rb
@@ -16,11 +16,15 @@ end
 def create_profile
   puts "What is your full name?"
   name = gets.chomp
-  User.find_or_create_by(name)
-  puts "Hi, #{user_name.name}"
+  User.create(name: name)
+  puts "Hi, #{name}"
 end
 
 def add_new_coffee
+  puts "Who drank this coffee? (Please enter name)"
+  user = gets.chomp
+  user_id = User.find_by(name: user).id
+
   puts "Please enter a name"
   name = gets.chomp
 
@@ -28,7 +32,7 @@ def add_new_coffee
   location = gets.chomp
 
   puts "How much did it cost?"
-  cost = gets.chomp
+  cost = gets.chomp.to_f
 
   puts "Please rate from 1-5"
   rating = gets.chomp
@@ -37,7 +41,21 @@ def add_new_coffee
   taste = gets.chomp
 
   Coffee.create(name: name, location: location, cost: cost, rating: rating, taste: taste)
+  My_Coffee.create(user_id: user_id, coffee_id: Coffee.last.id)
   puts "Thanks for letting me know!"
+end
+
+def see_all_coffees
+  puts "What is your name?"
+  user_input = gets.chomp
+
+  user = User.find_by(name: user_input)
+  binding.pry
+
+  puts "Here are your coffees:"
+  user.coffee_names.each do |coffee_name|
+    puts "#{coffee_name}"
+  end
 end
 
 def add_to_favorites
@@ -47,24 +65,13 @@ def add_to_favorites
   puts "Please enter the coffee's name"
   coffee_input = gets.chomp
 
-  user = User.find_by(name: coffee_input)
+  user = User.find_by(name: user_input)
 
   user.coffees.each do |coffee|
     if coffee.name == coffee_input
-      coffee.favorite = true
+      coffee.favorites = true
     end
   end
-end
-
-def see_all_coffees
-  puts "What is your name?"
-  user_input = gets.chomp
-
-  user = User.find_by(name: user_input)
-
-  puts "Here are your coffees"
-
-  user.coffee_names
 end
 
 def bring_up_favorites
@@ -75,13 +82,14 @@ def bring_up_favorites
 
   puts "You love this coffee:"
 
-  user.coffee_names.each do |coffee_name|
-    if
-    puts "#{coffee_name}"
-  end
+  user.coffees.select  {|coffee_name| coffee.favorites == true}.map {|fave_coffee| fave_coffee.name}
 end
 
 welcome
-
+# create_profile
+# add_new_coffee
+# see_all_coffees
+# add_to_favorites
+bring_up_favorites
 binding.pry
 0
