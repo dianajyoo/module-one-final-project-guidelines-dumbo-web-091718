dbd5d269fdbe44012c2f9b972f0f0c74c825ada2
dianajyoo <diana.yooj@gmail.com>
2018-10-03 14:54:50 -0400

    got add_new_coffee to finally work

--- lib/methods.rb
+++ lib/methods.rb
@@ -32,45 +32,40 @@ def get_login
 
 end
 
-
-def return_to_menu
-
-  if $name == "menu"
-    menu
-  else
-    welcome
-  end
-
-end
-
 def get_user
   User.find_by(name: $name, password: $password)
 end
 
 def add_new_coffee
+  menu_message
   user_id = User.find_by(name: $name, password: $password).id
-  inputs = []
 
   request = ["Please enter a coffee name", "Where did you get it?", "How did it taste?", "How much did it cost? (Enter Number)"]
-  request.map do |request|
+  array = request.map do |request|
     puts request
-    input = gets.chomp
-    until input.empty? == false
-      puts "Please enter a valid input"
-      input = gets.chomp
-    end
-    inputs << input
+    input = gets.chomp.downcase
 
-    while inputs.count > 3 && inputs[3].to_f == 0
-      puts "Please enter a valid number for cost"
-      inputs[3] = gets.chomp
+    if input == 'menu'
+      welcome
+      break
+    else
+      until input.empty? == false
+        puts "Please enter a valid input"
+        input = gets.chomp
+      end
     end
+    input
   end
-
-  Coffee.create(name: capitalize(inputs[0]), shop_name: capitalize(inputs[1]), taste: inputs[2], cost: inputs[3])
-  MyCoffee.create(user_id: user_id, coffee_id: Coffee.last.id)
-  puts "Thanks for letting me know!"
-  welcome
+  if array.count == 4
+    while array[3].to_f == 0
+      puts "Please enter a valid number for cost"
+      array[3] = gets.chomp
+    end
+    Coffee.create(name: capitalize(array[0]), shop_name: capitalize(array[1]), taste: array[2], cost: array[3])
+    MyCoffee.create(user_id: user_id, coffee_id: Coffee.last.id)
+    puts "Thanks for letting me know!"
+    welcome
+  end
 end
 
 def see_all_coffees
@@ -78,40 +73,45 @@ def see_all_coffees
   if get_user.coffee_names.empty?
     puts "No coffees!"
   else
-    puts "Here are your coffees:"
+    puts "Here are your coffees:", ""
     get_user.coffee_names.each {|coffee_name| puts "#{coffee_name}"}
   end
   welcome
 end
 
 def add_to_favorites
+  menu_message
   puts "Please enter the coffee's name"
   coffee_input = gets.chomp.downcase
   if coffee_input == "menu"
-    menu
-  end
-
-  get_user.coffees.each do |coffee|
-    if coffee.name.downcase == coffee_input
-      coffee.favorites = true
-      coffee.save
-      puts "Added to favorites!"
+    welcome
+  else
+    get_user.coffees.each do |coffee|
+      if coffee.name.downcase == coffee_input
+        coffee.favorites = true
+        coffee.save
+        puts "Added to favorites!"
+      else
+        puts "", "Coffee not found"
+        break
+      end
     end
+    welcome
   end
-  welcome
 end
 
 def bring_up_favorites
-  puts "You love this coffee:"
+  puts "You love this coffee(s):", ""
   get_user.coffees.select {|coffee_name| coffee_name.favorites == true}.each {|fave_coffee| puts fave_coffee.name}
   welcome
 end
 
 def suggestion
+  puts menu_message
   puts "How much do you want to pay? (Enter Number)"
   cost = gets.chomp
   if cost == "menu"
-    menu
+    welcome
   else
     until cost.to_f > 0.0
       puts "Please enter a valid number for cost"
@@ -134,54 +134,52 @@ def suggestion
     if option == "y"
       suggestion.save
 
-      puts "Who is saving this coffee? (Please enter name)"
-      user = gets.chomp.downcase
-      user_id = User.find_by(name: user).id
+      user_id = User.find_by(name: $name, password: $password).id
 
       MyCoffee.create(user_id: user_id, coffee_id: Coffee.last.id)
       puts "Saved"
     else
       puts "It vanished!"
     end
+    welcome
   end
-  welcome
 end
 
 def search_coffees
 
   inputs = []
+  menu_message
 
   requests = ["What coffee would you like to search", "Which coffee shop?"]
-  requests.map do |request|
+  requests.each do |request|
     puts request
-    input = gets.chomp
-    until input.empty? == false
-      puts "Please enter a valid input"
-      input = gets.chomp
+    input = gets.chomp.downcase
+    if input == 'menu'
+        welcome
+        break
+    else
+      until input.empty? == false
+        puts "Please enter a valid input"
+        input = gets.chomp
+      end
     end
     inputs << input
   end
 
+    if inputs.count == 2
+      found_coffee = Coffee.find_by(name: capitalize(inputs[0]), shop_name: capitalize(inputs[1]))
 
-  # coffee = gets.chomp
-  # if coffee == "menu"
-  #   menu
-  # end
-  #
-  # if coffee != "menu"
-    found_coffee = Coffee.find_by(name: capitalize(inputs[0]), shop_name: capitalize(inputs[1]))
-
-    if found_coffee == nil
-      puts "Sorry, no coffee by that name!"
-    else
-      puts "
-            Name: #{found_coffee.name}
-            Shop Name: #{found_coffee.shop_name}
-            Price: $#{found_coffee.cost}
-            "
+      if found_coffee == nil
+        puts "Sorry, no coffee by that name!"
+      else
+        puts "
+              Name: #{found_coffee.name}
+              Shop Name: #{found_coffee.shop_name}
+              Price: $#{found_coffee.cost}
+              "
+      end
+      welcome
     end
-  # end
-  welcome
 end
 
 def capitalize(name)
@@ -192,12 +190,16 @@ def capitalize(name)
   end
 end
 
-def menu
-  puts "Would you like to go back to the menu? (Y/N)"
-  input = gets.chomp.downcase
-  if input == "y"
-    welcome
-  end
+# def menu(method, answer)
+#   if answer == "y"
+#     welcome
+#   elsif answer == "n"
+#     send(method)
+#   end
+# end
+
+def menu_message
+  puts "If you would like to return to menu, please enter 'menu'"
 end
 
 def run
